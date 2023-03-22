9afae5ed26e75191979817dc3a6229894220747b
Yernemm <yrnemmizer@gmail.com>
2023-03-22 16:34:55 +0000

    1.1: Mod updater

--- download.py
+++ download.py
@@ -1,6 +1,6 @@
 # BoneLab Mod.IO Downloader
 # By Yernemm
-# Version 1.0
+# Version 1.1
 
 import json
 import os
@@ -13,9 +13,20 @@ repos = ["https://blrepo.laund.moe/repository.json", "https://blrepo.laund.moe/n
 print("Getting mod folder path...")
 modFolder = os.getenv('APPDATA') + "\\..\\LocalLow\\Stress Level Zero\\Bonelab\\Mods"
 
+print("Getting mod info file...")
+modInfoPath = os.getenv('APPDATA') + "\\..\\LocalLow\\Stress Level Zero\\Bonelab\\yernemm_modio_downloader.json"
+#create file if it doesn't exist
+if not os.path.exists(modInfoPath):
+    print("Creating mod info file...")
+    with open(modInfoPath, 'w') as file:
+        file.write('{"version": "1.1", "installed": []}')
+        file.close()
+
+modInfo = json.load(open(modInfoPath))
+
 # Read installedinfo.json
-print("Checking previously downloaded mods...")
-installed = json.load(open("installedinfo.json"))
+#print("Checking previously downloaded mods...")
+#installed = json.load(open("installedinfo.json"))
 
 # Read modlist.txt
 print("Reading mod list...")
@@ -24,96 +35,200 @@ with open('modlist.txt') as file:
     for line in file:
         modlist.append(line.strip())
 
+notFoundList = []
+
 print("Mods in list: " + str(len(modlist)))
 #remove duplicates
 modlist = list(dict.fromkeys(modlist))
 print("Mods in list (no duplicates): " + str(len(modlist)))
 
-allMods = []
+def main():
+    print("===========================")
+    print("BoneLab Mod.IO Downloader by Yernemm")
+    print("Choose an option:")
+    mode = -1
+    while mode < 0 or mode > 3:
+        print("   1. Update existing list mods and install new ones")
+        print("   2. Install all mods from list")
+        print("   3. Generate mod list from installed mods")
+        print("   0. Exit")
+        try:
+            mode = int(input("Option: "))
+        except:
+            print("Invalid input.")
 
-# Read repository.json
-for repo in repos:
-    print("Downloading mod repository...")
-    repo = json.load(urllib.request.urlopen(repo))
-    print("Repo loaded.")
-    print("\"" + repo["objects"]["o:1"]["description"] + "\"")
+    print(" ")
 
-    mods = repo["objects"]["o:1"]["mods"]
-
-    print("Mods in repo: " + str(len(mods)))
-
-    print("Parsing mods...")
-
-
-    for i, mod in enumerate(mods):
-        modByRef = repo["objects"][str(mod["ref"])]
+    if mode == 1:
+        mode1()
+    elif mode == 2:
+        mode2()
+    elif mode == 3:
+        mode3()
+    
         
-        if("pc" not in modByRef["targets"]):
-            continue
-        else:
-            allMods.append(
-                    {
-                    "mod": modByRef,
-                    "dl": repo["objects"][str(modByRef["targets"]["pc"]["ref"])]
-                    }
-                )
+    print("Done.")
 
-            print("\r"+str(i + 1)+"/"+str(len(mods)), end="")
 
-    print("")
-    #allMods = [*allMods, *mods]
 
-print("Total PC mods: " + str(len(allMods)))
+def mode1():
+    print("Update existing list mods and install new ones")
+    allMods = getRepoMods()
+    purgeUpdatedMods(allMods)
+    zipFiles = downloadMods(allMods)
+    installMods(zipFiles)
+    saveInfo()
+    cleanUp(zipFiles)
+    checkNotFound()
+
+def mode2():
+    print("Install all mods from list")
+    allMods = getRepoMods()
+    zipFiles = downloadMods(allMods)
+    installMods(zipFiles)
+    cleanUp(zipFiles)
+    checkNotFound()
+
+def mode3():
+    print("Generate mod list from installed mods")
+    print("NOT IMPLEMENTED YET")
+
+
+def getRepoMods():
+    allMods = []
+    # Read repository.json
+    for repo in repos:
+        print("Downloading mod repository...")
+        repo = json.load(urllib.request.urlopen(repo))
+        print("Repo loaded.")
+        print("\"" + repo["objects"]["o:1"]["description"] + "\"")
+
+        mods = repo["objects"]["o:1"]["mods"]
+
+        print("Mods in repo: " + str(len(mods)))
+
+        print("Parsing mods...")
+
+
+        for i, mod in enumerate(mods):
+            modByRef = repo["objects"][str(mod["ref"])]
+            
+            if("pc" not in modByRef["targets"]):
+                continue
+            else:
+                allMods.append(
+                        {
+                        "mod": modByRef,
+                        "dl": repo["objects"][str(modByRef["targets"]["pc"]["ref"])]
+                        }
+                    )
+
+                print("\r"+str(i + 1)+"/"+str(len(mods)), end="")
+
+        print("")
+        #allMods = [*allMods, *mods]
+
+    print("Total PC mods: " + str(len(allMods)))
+    return allMods
 
 #print(repo["objects"])
 
-#make tmp folder if it doesn't exist
-if not os.path.exists(".\\tmp"):
-    os.mkdir(".\\tmp")
+def purgeUpdatedMods(allMods):
+    global modlist
+    print("Purging up-to-date mods from list...")
+    for i, mod in enumerate(allMods):
+        if mod["mod"]["barcode"] in modlist:
+            #check if mod is installed
+            barcode = mod["mod"]["barcode"]
+            url = mod["dl"]["url"]
+            if checkIfUpdated(barcode, url):
+                print("Mod " + barcode + " is up-to-date.")
+                modlist.remove(barcode)
+    
+                
 
-zipFiles = []
 
-counter = 0
-initModListSize = len(modlist)
+def downloadMods(allMods):
+    global modlist
+    global notFoundList
+    #make tmp folder if it doesn't exist
+    if not os.path.exists(".\\tmp"):
+        os.mkdir(".\\tmp")
+
+    zipFiles = []
+
+    counter = 0
+    initModListSize = len(modlist)
 
 
-print("Get all mods from mod list...")
-for i, mod in enumerate(allMods):
-    if mod["mod"]["barcode"] in modlist:
-        counter += 1
+    print("Downloading mods...")
+    for i, mod in enumerate(allMods):
+        if mod["mod"]["barcode"] in modlist:
+            counter += 1
 
-        modTitle = mod["mod"]["title"]
-        modLink = mod["dl"]["url"]
+            modTitle = mod["mod"]["title"]
+            modLink = mod["dl"]["url"]
 
-        print("Downloading " + modTitle + "...")
-        
-        print(modLink)
-        zipFile = ".\\tmp\\" + mod["mod"]["barcode"] + ".zip"
-        urllib.request.urlretrieve(modLink, zipFile)
-        zipFiles.append(zipFile)
-        print(str(counter) + "/" + str(initModListSize) + " downloaded.")
-        modlist.remove(mod["mod"]["barcode"])
-        #print("Downloaded " + mod["name"] + ".")
-        #installed.append(mod["name"])
-       # print("\r"+str(i + 1)+"/"+str(len(allMods)), end="")
+            print("Downloading " + modTitle + "...")
+            
+            print(modLink)
+            zipFile = ".\\tmp\\" + mod["mod"]["barcode"] + ".zip"
+            urllib.request.urlretrieve(modLink, zipFile)
+            zipFiles.append(zipFile)
+            print(str(counter) + "/" + str(initModListSize) + " downloaded.")
+            modlist.remove(mod["mod"]["barcode"])
 
-print("Installing mods...")
-for i, zipFile in enumerate(zipFiles):
-    print("Installing " + zipFile + "...")
-    with zipfile.ZipFile(zipFile, 'r') as zip_ref:
-        zip_ref.extractall(modFolder)
-    print(str(i + 1) + "/" + str(len(zipFiles)) + " installed.")
+            addInstalledMods(mod["mod"]["barcode"], modLink)
+            #print("Downloaded " + mod["name"] + ".")
+            #installed.append(mod["name"])
+        # print("\r"+str(i + 1)+"/"+str(len(allMods)), end="")
+    notFoundList = modlist
+    modlist = []
+    return zipFiles
+
+def installMods(zipFiles):
+    print("Installing mods...")
+    for i, zipFile in enumerate(zipFiles):
+        print("Installing " + zipFile + "...")
+        with zipfile.ZipFile(zipFile, 'r') as zip_ref:
+            zip_ref.extractall(modFolder)
+        print(str(i + 1) + "/" + str(len(zipFiles)) + " installed.")
 
 # Clean up
 
-print("Cleaning up...")
-for zipFile in zipFiles:
-    os.remove(zipFile)
+def cleanUp(zipFiles):
+    print("Cleaning up...")
+    for zipFile in zipFiles:
+        os.remove(zipFile)
 
-print("Done.")
 
+def checkNotFound():
+    if(len(notFoundList) > 0):
+        print("The following mods were not found:")
+        for mod in notFoundList:
+            print("   " + mod)
+ 
+def saveInfo():
+    global modInfo
+    global modInfoPath
+    print("Logging installed mods...")
+    with open(modInfoPath, 'w') as file:
+        json.dump(modInfo, file, indent=4)
+        file.close()
 
-if(len(modlist) > 0):
-    print("The following mods were not found:")
-    for mod in modlist:
-        print(mod)
+def addInstalledMods(barcode, url):
+    global modInfo
+    modInfo["installed"].append({"barcode": barcode, "url": url})
+    saveInfo()
+
+def checkIfUpdated(barcode, url):
+    global modInfo
+    for mod in modInfo["installed"]:
+        if mod["barcode"] == barcode:
+            if mod["url"] == url:
+                return True
+            else:
+                return False
+    return False
+
+main()
